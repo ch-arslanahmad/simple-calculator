fe611e1ce7b7a0addf2dd2621edbea370798a6fd
Muhammad Arslan <ch.arslanad@gmail.com>
2025-08-02 21:30:26 +0500

    feat(Calculator.java): now using logging

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,13 +1,16 @@
+// imports
+import java.util.logging.*;
 import java.util.Scanner;
+// Calculator
 class Calculator {
-    float a;
-    float b;
-    int c;
-    Calculator(float x, float y, int z) {
-        this.a = x;
-        this.b = y;
-        this.c = z;
-    }
+    // private variables
+    private float a;
+    private float b;
+    private int c;
+    // private logger object
+    private static final Logger logger = Logger.getLogger(Calculator.class.getName());
+
+    // input method
     void getInput(Scanner input) {
         System.out.print("Enter 1st value: ");
         a = input.nextFloat();
@@ -17,22 +20,50 @@ class Calculator {
         System.out.print("Enter choice: ");
         c = input.nextInt();
     }
+
+    // add method
     void add() {
         System.out.println("The sum is " + (a+b));
     }
+
+    // subtract method
     void subtract() {
         System.out.println("The subtraction is " + (a-b));
         
     }
+    // multiplication method
     void multiply() {
         System.out.println("The multiplication is " + (a*b));
     }
-    
+    // division method
     void divide() {
+        // try-catch
+        try {
+            // conditional
+            if(b == 0) {
+            // filehandler to create a log file
+            FileHandler fh = new FileHandler("OOP-tasks/week#6/task#3/log.txt", true);
+            // formatter to format it into simple, not XML etc
+            fh.setFormatter(new SimpleFormatter());
+            // adding handler
+            logger.addHandler(fh);
+
+            // setting logger level: INFO: permits all messages
+            logger.setLevel(Level.INFO);
+            // print (& write) these messages 
+            logger.severe("ILLEGAL Operation");
+            logger.info("Divisor(b) shouldn't be zero.");
+
+            }
+        } catch (Exception e) {
+            System.out.println("Error Occured.");
+            e.printStackTrace();
+        }
         System.out.println("The division is " + (a/b));
     }
     
-    void Choose() {
+    // choose method
+    void choose() {
         switch (c) {
             case 1:
                 add();
@@ -52,12 +83,12 @@ class Calculator {
     }
 
     public static void main(String[] args) {
-        float a = 0;
-        float b = 0;
-        int c = 0;
+        // Scanner object for input
         Scanner input = new Scanner(System.in);
-        Calculator calculator = new Calculator(a,b,c);
+        // calculator object
+        Calculator calculator = new Calculator();
+        // running the methods.a
         calculator.getInput(input);
-        calculator.Choose();
+        calculator.choose();
     }
 }
